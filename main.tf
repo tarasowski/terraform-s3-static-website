76fc8c3cf17d872157d3e13fd597915dad14c771
tarasowski <dimitri+github@tarasowski.de>
2025-02-12 06:52:06 +0100

    fix: formatted

--- main.tf
+++ main.tf
@@ -19,18 +19,18 @@ resource "aws_s3_bucket_website_configuration" "site" {
   bucket = aws_s3_bucket.site.id
 
   index_document {
-  suffix = "index.html"
+    suffix = "index.html"
   }
 
   error_document {
-  key = "error.html"
+    key = "error.html"
   }
 }
 
 resource "aws_s3_bucket_ownership_controls" "site" {
   bucket = aws_s3_bucket.site.id
   rule {
-  object_ownership = "BucketOwnerPreferred"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
@@ -39,8 +39,8 @@ resource "aws_s3_bucket_acl" "site" {
 
   acl = "public-read"
   depends_on = [
-  aws_s3_bucket_ownership_controls.site,
-  aws_s3_bucket_public_access_block.site
+    aws_s3_bucket_ownership_controls.site,
+    aws_s3_bucket_public_access_block.site
   ]
 }
 
@@ -48,23 +48,23 @@ resource "aws_s3_bucket_policy" "site" {
   bucket = aws_s3_bucket.site.id
 
   policy = jsonencode({
-  Version = "2012-10-17"
-  Statement = [
-    {
-    Sid       = "PublicReadGetObject"
-    Effect    = "Allow"
-    Principal = "*"
-    Action    = "s3:GetObject"
-    Resource = [
-      aws_s3_bucket.site.arn,
-      "${aws_s3_bucket.site.arn}/*",
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.site.arn,
+          "${aws_s3_bucket.site.arn}/*",
+        ]
+      },
     ]
-    },
-  ]
   })
 
   depends_on = [
-  aws_s3_bucket_public_access_block.site
+    aws_s3_bucket_public_access_block.site
   ]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "site_domain" {
-  default = "www.tarasowski.com"
+  default     = "www.tarasowski.com"
   description = "Add the website name here"
 }
 
 variable "aws_region" {
-  default = "us-east-1"
+  default     = "us-east-1"
   description = "Add the region for the website"
 }
